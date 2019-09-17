c1bc8b220b53dc3b7f636509ea7d9c36305eb832
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-17 00:02:29 -0400

    Se agrego al formulario en el select las opciones de eventos

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,15 +2,45 @@ import React, { Component } from 'react';
 
 class Formulario extends Component {
 
+	//Creando los ref
+	eventoRef = React.createRef();
+
+	mostrarOpciones = key =>{
+		const categoria = this.props.categorias[key];
+		console.log(categoria);
+		const {id, name_localized} = categoria;
+		if(!id || !name_localized){
+			return null;
+		}
+		return(
+			<option key = {id} value = {id}>{name_localized}</option>
+		)
+	}
+	
+
 	state = {  }
 
-	render() { 
+	render() {
+		const categorias = Object.keys(this.props.categorias);
 		return (
 				<form>
 					<fieldset className = "uk-fieldset uk-margin">
 						<legend className="uk-legend uk-text-center">
 							Busca tu evento por nombre o categoria
 						</legend>
+						<div className ="uk-column-1-3@m uk-margin">
+							<div className ="uk-margin" uk-margin ="true">
+								<input className =" uk-input" type = "text" placeholder = "Nombre de evento o ciudad"></input>
+							</div>
+							<div className ="uk-margin" uk-margin ="true">
+								<select className ="uk-select">
+									{categorias.map(this.mostrarOpciones)}
+								</select>
+							</div>
+							<div className ="uk-margin" uk-margin ="true">
+								<button className =" uk-button uk-button-danger">Buscar</button>
+							</div>
+						</div>
 					</fieldset>
 				</form>
 		);
